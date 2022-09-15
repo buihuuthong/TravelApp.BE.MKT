1e34b84d51f823734ac5ef6a4dabf3e6e61577b2
buihuuthong <buihuuthong2806@gmail.com>
2022-09-15 10:41:09 +0700

    fix structurecode

---
+++ src/main/java/com/MyTravel/mytravel/MytravelApplication.java
@@ -0,0 +1,13 @@
+package com.MyTravel.mytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyTravel/mytravel/MytravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyTravel.mytravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
